56a7689217e1227df914586c92725f8ee296a2d5
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2024-02-29 23:32:16 +0100

    Basic project structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::prelude::{CrosstermBackend, Terminal};
+use std::io::{stdout, Result};
+
+mod data;
+mod ui;
+
+struct App {
+    screen: Box<dyn ui::Screen>,
+}
+
+fn main() -> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    let app = App {
+        screen: Box::new(ui::screen::TestScreen::new()),
+    };
+
+    loop {
+        terminal.draw(|frame| {
+            app.screen.draw(frame);
+        })?;
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
