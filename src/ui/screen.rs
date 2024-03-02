bed19055e86bbda203919a5955342d6e9ec8cb9f
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2024-03-02 09:58:19 +0100

    Matched signature of draw call to ratatui widgets

--- src/ui/screen.rs
+++ src/ui/screen.rs
@@ -1,12 +1,17 @@
-mod main_screen;
-pub use main_screen::MainScreen;
+use ratatui::{buffer, layout};
+
+mod select_screen;
+pub use select_screen::SelectScreen;
+
+mod stats_screen;
+pub use stats_screen::StatsScreen;
 
 use crate::ui::input;
 
 /// A trait that is implemented by different screens within the application.
 pub trait Screen {
     /// Draws the screen to the frame (taking all the available space).
-    fn draw(&self, frame: &mut ratatui::Frame);
+    fn draw(&self, area: layout::Rect, buf: &mut buffer::Buffer);
 
     /// Informs the screen of user messages and possibly modifies the content.
     fn update(&mut self, msg: input::Message) -> Option<input::Message>;
--- src/ui/screen/select_screen.rs
+++ src/ui/screen/select_screen.rs
@@ -3,22 +3,29 @@ use std::collections::HashMap;
 use crate::data::Note;
 use ratatui::{prelude::*, widgets::*};
 
-pub struct MainScreen {
+pub struct SelectScreen {
     index: HashMap<String, Note>,
 }
 
-impl MainScreen {
+impl SelectScreen {
     pub fn new(index: HashMap<String, Note>) -> Self {
         Self { index }
     }
 }
 
-impl super::Screen for MainScreen {
-    fn draw(&self, frame: &mut Frame) {
+impl super::Screen for SelectScreen {
+    fn update(&mut self, msg: crate::ui::input::Message) -> Option<crate::ui::input::Message> {
+        match msg {
+            crate::ui::input::Message::Clear => self.index.clear(),
+            m => return Some(m),
+        }
+        None
+    }
+
+    fn draw(&self, area: layout::Rect, buf: &mut buffer::Buffer) {
         // TODO: Layouting
         // TODO: Draw signature as in tutorial
         // TODO: Stateful list
-        let area = frame.size();
         let ar1 = ratatui::layout::Rect {
             y: area.height / 4,
             height: area.height * 3 / 4,
@@ -31,6 +38,8 @@ impl super::Screen for MainScreen {
 
         let vec = self.index.clone().into_keys().collect::<Vec<String>>();
 
+        let mut state = ListState::default();
+
         let list = List::new(vec)
             .block(Block::bordered().title("Notes"))
             .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
@@ -38,19 +47,13 @@ impl super::Screen for MainScreen {
             .repeat_highlight_symbol(true)
             .direction(ListDirection::BottomToTop);
 
-        frame.render_widget(list, ar1);
-        frame.render_widget(
+        StatefulWidget::render(list, ar1, buf, &mut state);
+
+        Widget::render(
             ratatui::widgets::Paragraph::new(format!("Lots of info"))
                 .block(Block::bordered().title("Statistics")),
             ar2,
+            buf,
         );
-    }
-
-    fn update(&mut self, msg: crate::ui::input::Message) -> Option<crate::ui::input::Message> {
-        match msg {
-            crate::ui::input::Message::Clear => self.index.clear(),
-            m => return Some(m),
-        }
-        None
     }
 }
